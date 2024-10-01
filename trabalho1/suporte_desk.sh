1a803123028d54bef2b162f318e28d563127a255
Raúl Beijinho <raulbeijinho108@gmail.com>
2024-10-01 15:58:57 +0100

    trabalho1, suporte_desk

--- trabalho1/suporte_desk.sh
+++ trabalho1/suporte_desk.sh
@@ -1,3 +1,21 @@
 PIPE_NAME="/tmp/suporte"
 
+if [[ ! -p $PIPE_NAME ]]; then
+mkfifo $PIPE_NAME
+fi
 
+for i in {1..3}; do
+ 
+ ./student "pedido $i"
+
+done
+
+./student_agente.sh 
+
+sleep 1
+
+echo "quit" > $PIPE_NAME
+
+wait
+
+rm $PIPE_NAME
