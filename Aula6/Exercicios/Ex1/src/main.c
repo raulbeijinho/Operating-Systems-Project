a6917348c6da63ca427f14760d13b950a23c2c71
Raúl Beijinho <raulbeijinho108@gmail.com>
2024-10-24 11:15:15 +0100

    adwad

--- Aula6/Exercicios/Ex1/src/main.c
+++ Aula6/Exercicios/Ex1/src/main.c
@@ -1,10 +1,13 @@
-int le_pipe (int fd, char *msg_in, int bsize)
+int le_pipe(int fd, char *msg_in, int bsize)
 {
     int j;
-    for(j=0; j<bsize; j++) msg_in[j]=0;
-    for(j=0; j<bsize; j++) {
-        if (read (fd, &msg_in[j], 1) < 0) {
-            perror ("le_pipe read");
+    for (j = 0; j < bsize; j++)
+        msg_in[j] = 0;
+    for (j = 0; j < bsize; j++)
+    {
+        if (read(fd, &msg_in[j], 1) < 0)
+        {
+            perror("le_pipe read");
             exit(1);
         }
         if (msg_in[j] == '\0')
